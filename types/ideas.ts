3ee8c25f07cbe97aeb9f0ec5aba9e7804c3e29ba
omarkhair70-droid <omar.khair70@gmail.com>
2026-04-30 01:08:20 +0300

    Make idea generator input aware

--- types/ideas.ts
+++ types/ideas.ts
@@ -1,4 +1,32 @@
+import type {
+  BudgetLevel,
+  Department,
+  ExperienceLevel,
+  Mood,
+  ProjectFormat
+} from "@/types/taxonomy";
+
+export interface IdeaInput {
+  department: Department;
+  projectFormat: ProjectFormat;
+  mood: Mood;
+  budgetLevel: BudgetLevel;
+  experienceLevel: ExperienceLevel;
+}
+
+export interface IdeaItem {
+  id: string;
+  title: string;
+  concept: string;
+  materials: string[];
+  executionMethod: string[];
+  whyStrong: string;
+  shortExplanation: string;
+  juryDefenseLine: string;
+  fitScore: number;
+}
+
 export interface IdeaOutput {
-  seed: string;
-  variation: string;
+  summary: string;
+  ideas: IdeaItem[];
 }
